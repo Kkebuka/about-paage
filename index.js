6e1e8d98d4e3d9e866b4b4a0caa924ad9a650632
kkebuka <kkebuka@gmail.com>
2024-02-05 17:24:27 +0100

    more changes

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 const histor = document.querySelector('.history');
 const vision = document.querySelector('.vision');
 const goals = document.querySelector('.goals');
+
 const historyLink = document.querySelector('.history1');
 const visionLink = document.querySelector('.vision1');
 const goalsLink = document.querySelector('.goals1');
+
 const historyText = document.querySelector('.history-stat');
 const visionText = document.querySelector('.vision-stat');
 const goalsText = document.querySelector('.goals-stat');
 
-visionText.style.display = 'none';
-goalsText.style.display = 'none';
-historyText.style.display ='block'
-histor.addEventListener('click', function(){
-    if(visionText.style.display ==='block' || goalsText.style.display ==='block'){
-        visionText.style.display ='none'
-        goalsText.style.display ='none'
-        historyText.style.display ='block'
-        historyLink.style.backgroundColor = 'white'
-    }else{}
-});
+
+
+
 
 vision.addEventListener('click', function(){
-    console.log(visionText);
-    console.log(historyText)
-    if(historyText.style.display ==='block' || goalsText.style.display ==='block' ){
-        historyText.style.display ='none';
-        goalsText.style.display ='none';
-        visionText.style.display ='block';
-        visionLink.style.backgroundColor = 'white'
-    }else{}
+   historyLink.style.backgroundColor = 'rgb(184, 209, 231)';
+   goalsLink.style.backgroundColor = 'rgb(184, 209, 231)';
+   historyText.classList.add('newstat-prop');
+   goalsText.classList.add('newstat-prop');
+   visionLink.style.backgroundColor = 'white';
+   visionText.classList.remove('newstat-prop')
+
+});
+visionLink.addEventListener('mouseover', function(){
+    visionLink.style.backgroundColor = 'rgb(217, 235, 250)';
 });
 
 goals.addEventListener('click', function(){
-    if(visionText.style.display ==='block' || historyText.style.display ==='block'){
-        visionText.style.display ='none'
-        historyText.style.display ='none'
-        goalsText.style.display ='block'
-    }else{}
+    historyLink.style.backgroundColor = 'rgb(184, 209, 231)';
+    visionLink.style.backgroundColor = 'rgb(184, 209, 231)';
+    historyText.classList.add('newstat-prop');
+    visionText.classList.add('newstat-prop');
+    goalsLink.style.backgroundColor = 'white';
+    goalsText.classList.remove('newstat-prop');
+});
+
+
+histor.addEventListener('click', function(){
+    goalsLink.style.backgroundColor = 'rgb(184, 209, 231)';
+    visionLink.style.backgroundColor = 'rgb(184, 209, 231)';
+    goalsText.classList.add('newstat-prop');
+    visionText.classList.add('newstat-prop');
+    historyLink.style.backgroundColor = 'white';
+    historyText.classList.remove('newstat-prop');
 });
